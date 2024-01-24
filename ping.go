fb2109ed384cb2d9b278421d87888e2cca50c545
Richa Yadav <richa.10603@gmail.com>
2024-01-24 11:32:42 +0530

    server finding

--- ping.go
+++ ping.go
@@ -16,6 +16,76 @@ const (
 	maxHops = 10
 	numThreads = 3
 )
+
+
+func find_server(test_name string, filter_map map[string]string) string {
+	localIPv4 := utils.GetLocalIP("v4")
+	localIPv6 := utils.GetLocalIP("v6")
+	//packet capture params
+	var snaplen int32 = 96
+	num_pkts := 0
+	iface, err := utils.GetDefaultInterface()
+	fmt.Println("Interface is ", iface.Name)
+	if err != nil {
+	  fmt.Println("Failed to get default interface:", err)
+	  return ""
+	}
+  
+	capture_filter := filter_map[test_name]
+	handle, err := pcap.OpenLive(iface.Name, snaplen, false, pcap.BlockForever)
+	if err != nil {
+	  log.Fatal(err)
+	}
+  
+	defer handle.Close()
+  
+	// Set the capture filter
+	err = handle.SetBPFFilter(capture_filter)
+	if err != nil {
+	  log.Fatal(err)
+	}
+  
+	ipCountMap := make(map[string]int)
+	var localIP string
+	// Start capturing packets
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	var sourceIP string
+	var destIP string
+	var serverIP string
+	for packet := range packetSource.Packets() {
+  
+	  if utils.IsIPv6Packet(packet) {
+		ipPacket, _ := packet.Layer(layers.LayerTypeIPv6).(*layers.IPv6)
+		sourceIP = ipPacket.SrcIP.String()
+		destIP = ipPacket.DstIP.String()
+		localIP = localIPv6
+	  } else {
+		 ipPacket, _ := packet.Layer(layers.LayerTypeIPv4).(*layers.IPv4)
+		 sourceIP = ipPacket.SrcIP.String()
+		 destIP = ipPacket.DstIP.String()
+		 localIP = localIPv4
+	  }
+	  //fmt.Println(localIP, srcIP, destIP)
+	  if sourceIP == localIP {
+		serverIP = destIP
+	  } else {
+		serverIP = sourceIP
+	  }
+  
+	  if _, ok := ipCountMap[serverIP]; !ok {
+		ipCountMap[serverIP] = 0
+	  }
+	  ipCountMap[serverIP] += 1
+	  // Process captured packet
+	  num_pkts += 1
+	  if num_pkts == 1000 {
+		break
+	  }
+	}
+	serverIPMax := utils.GetKeyWithMaxValue(ipCountMap)
+	return serverIPMax
+}
+
 func getIP(ht string) (string, error) {
 	ip, err := net.LookupIP(ht)
 	if err != nil || len(ip) == 0 {
@@ -96,6 +166,13 @@ func main() {
 	} else {
 		targetIP = destination
 	}
+	var filter_map = map[string]string {
+		"mlab": "port 443",
+		"ookla": "port 8080 or port 5060",
+	  }
+	
+	var test_name = "mlab"
+	targetIP := find_server(test_name, filter_map)
 	//print target IP
 	fmt.Printf("Target IP: %s\n", targetIP)
 	var wg sync.WaitGroup
