58b7926f070b22d9191213a591f986ff6013d3b6
manshi137 <manshisagar@Manshis-MacBook-Air.local>
2024-02-22 19:30:35 +0530

    ref for ttest

--- ttest.py
+++ ttest.py
@@ -4,81 +4,75 @@ import numpy as np
 import scipy.stats as stats
 from datetime import datetime
 
+hops = 3
+alpha = 0.05
+
 def calculate_time_difference(request_time, reply_time):
     request_time = datetime.strptime(request_time, "%H:%M:%S.%f")
     reply_time = datetime.strptime(reply_time, "%H:%M:%S.%f")
     time_diff = reply_time - request_time
     return time_diff.total_seconds()
 
-hops = 3
-# read from ping_reply.csv
-# SequenceNumber,RequestTime,ReplyTime,RequestSourceIP,RequestDestIP,ReplySourceIP,ReplyDestIP,ReplyTTLexpiredIP,TTL,Download/Upload/Idle
-ping_reply_map = []
-download = [[] for i in range(hops+1)]
-upload = [[] for i in range(hops+1)]
-idle = [[] for i in range(hops+1)]
+def run_ttest():
+    # read from ping_reply.csv
+    ping_reply_map = []
+    download = [[] for i in range(hops+1)]
+    upload = [[] for i in range(hops+1)]
+    idle = [[] for i in range(hops+1)]
 
-with open('ping_reply.csv', 'r') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        request_time = row['RequestTime']
-        reply_time = row['ReplyTime']
-        time_diff = calculate_time_difference(request_time, reply_time)
-        # print(f"Time difference: {time_diff} seconds")
-        if(row['Download/Upload/Idle'] == 'download'):
-            download[int(row['TTL'])].append(time_diff)
-        elif(row['Download/Upload/Idle'] == 'upload'):
-            upload[int(row['TTL'])].append(time_diff)
-        elif(row['Download/Upload/Idle'] == 'idle'):
-            idle[int(row['TTL'])].append(time_diff)
-
-
-n = min(len(download), len(upload))
-n = min(n, len(idle))
-
-# truncate the list to the same length
-for h in range(1, hops+1):
-    download[h] = download[h][:n]
-    upload[h] = upload[h][:n]
-    idle[h] = idle[h][:n]
-    # print(f"download[{h}]: {len(download[h])}, upload[{h}]: {len(upload[h])}, idle[{h}]: {len(idle[h])}")
+    with open('ping_reply.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            request_time = row['RequestTime']
+            reply_time = row['ReplyTime']
+            time_diff = calculate_time_difference(request_time, reply_time)
+            # print(f"Time difference: {time_diff} seconds")
+            if(row['Download/Upload/Idle'] == 'download'):
+                download[int(row['TTL'])].append(time_diff)
+            elif(row['Download/Upload/Idle'] == 'upload'):
+                upload[int(row['TTL'])].append(time_diff)
+            elif(row['Download/Upload/Idle'] == 'idle'):
+                idle[int(row['TTL'])].append(time_diff)
 
 
-for hop in range(hops, 1, -1):
-    download[hop] = [download[hop][i] - download[hop-1][i] for i in range(n)]
-    upload[hop] = [upload[hop][i] - upload[hop-1][i] for i in range(n)]
-    idle[hop] = [idle[hop][i] - idle[hop-1][i] for i in range(n)]
+    n = min(len(download), len(upload))
+    n = min(n, len(idle))
 
-# download vs idle
-for hop in range(1, hops+1):
-    t_statistic, p_value = stats.ttest_ind(download[hop], idle[hop])
-    print("hop= ", hop, "download vs idle")
-    print("t_statistic= ", t_statistic)
-    print("p_value= ", p_value)
-    print("\n")
+    # truncate the list to the same length
+    for h in range(1, hops+1):
+        download[h] = download[h][:n]
+        upload[h] = upload[h][:n]
+        idle[h] = idle[h][:n]
+        # print(f"download[{h}]: {len(download[h])}, upload[{h}]: {len(upload[h])}, idle[{h}]: {len(idle[h])}")
 
-# upload vs idle
-for hop in range(1, hops+1):
-    t_statistic, p_value = stats.ttest_ind(upload[hop], idle[hop])
-    print("hop= ", hop, "upload vs idle")
-    print("t_statistic= ", t_statistic)
-    print("p_value= ", p_value)
-    print("\n")
+    # convert the cumulative time to time difference
+    for hop in range(hops, 1, -1):
+        download[hop] = [download[hop][i] - download[hop-1][i] for i in range(n)]
+        upload[hop] = [upload[hop][i] - upload[hop-1][i] for i in range(n)]
+        idle[hop] = [idle[hop][i] - idle[hop-1][i] for i in range(n)]
 
-upload_mean = []
-download_mean = []
-idle_mean = []
+    # download vs idle
+    for hop in range(1, hops+1):
+        t_statistic, p_value = stats.ttest_ind(download[hop], idle[hop])
+        print("hop= ", hop, "download vs idle")
+        print("t_statistic= ", t_statistic)
+        print("p_value= ", p_value)
+        print("\n")
 
-# find expectation value of download_diff and upload_diff for each hop
-# download_diff_mean = []
-# upload_diff_mean = []
-# for hop in range(hops):
-#     download_mean.append(np.mean(download[hop]))
-#     upload_mean.append(np.mean(upload[hop]))
-#     idle_mean.append(np.mean(idle[hop]))
+    print("------------------------------------------- \n")
+    # upload vs idle
+    for hop in range(1, hops+1):
+        t_statistic, p_value = stats.ttest_ind(upload[hop], idle[hop])
+        print("hop= ", hop, "upload vs idle")
+        print("t_statistic= ", t_statistic)
+        print("p_value= ", p_value)
+        print("\n")
 
-# for i in range(hops, 1, -1):
-#     download_mean.append(download_mean[i] - download_mean[i-1])
-#     upload_mean.append(upload_mean[i] - upload_mean[i-1])
-#     idle_mean.append(idle_mean[i] - idle_mean[i-1])
+    upload_mean = []
+    download_mean = []
+    idle_mean = []
+
+
+# run the ttest
+run_ttest()
 
