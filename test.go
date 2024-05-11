ed9c8e8ee3504c48492e51ff85d8ec5a0ae181bb
manshi137 <manshisagar@Manshis-MacBook-Air.local>
2024-05-11 23:07:15 +0530

    Revert "updated readme"
    This reverts commit 0f5e311d59b0baa26a709484a22a12a0ee77f94b.

--- test.go
+++ test.go
@@ -1,25 +1,70 @@
-package main 
+package main
 
-import ( 
+import (
 	"fmt"
+	// "net"
+	// "log"
+	// "github.com/google/gopacket"
+	// "github.com/google/gopacket/pcap"
+    // "github.com/google/gopacket/layers"
+	// "github.com/google/gopacket/pcapgo"
+	"os/exec"
+	// "sync"
 	"time"
-	"strconv"
-) 
+	// "regexp"
+	// "io/ioutil"
+	// "os"
+	// "./utils"
+)
+// import "github.com/manshi137/COD891/utils"
 
-func display(str string) { 
- 
-	fmt.Println(str) 
+
+
+func pingWithTTL(ttl int, targetIP string) {
+	numPacket := 1
 	
-} 
+	
+	npingCommand := fmt.Sprintf("ping -n %d -i %d  %s", numPacket, ttl, targetIP)
+	pingOutput1, err := exec.Command("cmd", "/C", npingCommand).Output()
+	if err != nil {
+		fmt.Println("Error executing ping:", err, ttl)
+		return
+	}
+	fmt.Printf("ping with TTL %d, TARGETIP= %s \n", ttl, targetIP)
+	fmt.Println("ping reply = ")
+	fmt.Println(string(pingOutput1))
+	// return 
+}
 
-func main() { 
-	time.Sleep(1 * time.Millisecond)
-	// Calling Goroutine 
-	for i := 0; i < 10; i++ {
-		time.Sleep(1 * time.Millisecond)
-		go display("Welcome" + strconv.Itoa(i))
-	}
-	for i := 0; i < 10; i++ {
-		display(strconv.Itoa(i))
-	}
-} 
+
+
+
+
+func main() {
+	targetIP := "2403:0:400:56::229"
+	// targetIP2 := "115.113.240.203"
+	ttl:=1
+	pingWithTTL(ttl, targetIP)
+	time.Sleep(1 * time.Second)
+	// pingWithTTL(ttl, targetIP2)
+	
+}
+// ping with TTL 1, TARGETIP= 115.113.240.203 
+// ping reply =
+
+// Pinging 115.113.240.203 with 32 bytes of data:
+// Reply from 10.194.32.13: TTL expired in transit.
+
+// Ping statistics for 115.113.240.203:
+//     Packets: Sent = 1, Received = 1, Lost = 0 (0% loss),
+
+// ---------------------------
+// ping with TTL 1, numPacket= 1 
+// ping reply = 
+
+// Pinging 2403:0:400:56::216 with 32 bytes of data:
+// Reply from 2403:0:400:56::216: TTL expired in transit.
+
+// Ping statistics for 2403:0:400:56::216:
+//     Packets: Sent = 1, Received = 1, Lost = 0 (0% loss),
+
